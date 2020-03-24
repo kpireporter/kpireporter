082363c9fd2ee2387e96642cbbbb9f3b6f9509ae
Jason <diurnalist@gmail.com>
2020-03-23 22:50:09 -0500

    Add tests for multiple sources/plugins

--- reportcard/tests/test_datasource.py
+++ reportcard/tests/test_datasource.py
@@ -14,12 +14,14 @@ PLUGIN = "my_plugin"
 class TestDatasource(unittest.TestCase):
 
     def setUp(self):
-        def extension_manager_factory(plugin=MagicMock()):
-            test_ext = stevedore.extension.Extension(
-                PLUGIN, entry_point=MagicMock(), plugin=plugin, obj=None)
+        def extension_manager_factory(plugins=[(PLUGIN, MagicMock())]):
+            extensions = [
+                stevedore.extension.Extension(
+                    name, entry_point=MagicMock(), plugin=plugin, obj=None)
+                for name, plugin in plugins]
 
             return stevedore.ExtensionManager.make_test_instance(
-                extensions=[test_ext]
+                extensions=extensions
             )
 
         self.extension_manager_factory = extension_manager_factory
@@ -28,9 +30,17 @@ class TestDatasource(unittest.TestCase):
         with self.assertRaises(DatasourceLoadError):
             DatasourceManager({DATASOURCE: {}})
 
+    def _make_datasource_with_plugins(self, plugins=None,
+                                      conf={DATASOURCE: {"plugin": PLUGIN}}):
+        factory = partial(self.extension_manager_factory,
+                          plugins=plugins)
+        return DatasourceManager(conf, extension_manager_factory=factory)
+
     def test_invalid_plugin(self):
         with self.assertRaises(DatasourceLoadError):
-            DatasourceManager({DATASOURCE: {"plugin": PLUGIN}})
+            DatasourceManager(
+                {DATASOURCE: {"plugin": "missing"}},
+            )
 
     def test_invalid_args(self):
         with self.assertRaises(DatasourceLoadError):
@@ -39,27 +49,19 @@ class TestDatasource(unittest.TestCase):
                 extension_manager_factory=self.extension_manager_factory
             )
 
-    def _make_datasource_with_plugin(self, plugin):
-        factory = partial(self.extension_manager_factory,
-                          plugin=plugin)
-        return DatasourceManager(
-            {DATASOURCE: {"plugin": PLUGIN}},
-            extension_manager_factory=factory
-        )
-
     def test_raises_on_invoke(self):
         def test_plugin():
             raise ValueError("I'm a bad plugin!")
 
         with self.assertRaises(DatasourceLoadError):
-            self._make_datasource_with_plugin(test_plugin)
+            self._make_datasource_with_plugins([(PLUGIN, test_plugin)])
 
     def test_invalid_query_return_type(self):
         class TestPlugin():
             def query(self, input):
                 return None
 
-        mgr = self._make_datasource_with_plugin(TestPlugin)
+        mgr = self._make_datasource_with_plugins([(PLUGIN, TestPlugin)])
 
         with self.assertRaises(DatasourceError):
             mgr.query(DATASOURCE, "some input")
@@ -71,6 +73,40 @@ class TestDatasource(unittest.TestCase):
             def query(self, input):
                 return df
 
-        mgr = self._make_datasource_with_plugin(TestPlugin)
+        mgr = self._make_datasource_with_plugins([(PLUGIN, TestPlugin)])
+
+        pd.testing.assert_frame_equal(df, mgr.query(DATASOURCE, "some input"))
+
+    def test_multiple_datasources(self):
+        df = pd.DataFrame()
+
+        class TestPlugin():
+            def query(self, input):
+                return df
+
+        mgr = self._make_datasource_with_plugins(
+            conf={
+                DATASOURCE: {"plugin": PLUGIN},
+                "second": {"plugin": PLUGIN},
+            },
+            plugins=[(PLUGIN, TestPlugin)])
+
+        pd.testing.assert_frame_equal(df, mgr.query("second", "some input"))
+
+    def test_multiple_plugins(self):
+        df = pd.DataFrame()
+
+        class FirstTestPlugin():
+            def query(self, input):
+                return None
+
+        class SecondTestPlugin():
+            def query(self, input):
+                return df
+
+        mgr = self._make_datasource_with_plugins([
+            ("first", FirstTestPlugin),
+            (PLUGIN, SecondTestPlugin)
+        ])
 
         pd.testing.assert_frame_equal(df, mgr.query(DATASOURCE, "some input"))

4d0065bfa2ddc13f7b4c9e8499865e9da69548b9
Jason <diurnalist@gmail.com>
2020-03-29 18:26:01 -0500

    More styling of plot defaults

--- reportcard/plugins/plot.py
+++ reportcard/plugins/plot.py
@@ -13,18 +13,46 @@ class Plot(View):
             raise ValueError((
                 "Both a 'datasource' and 'query' parameter are required"))
 
+    @property
+    def matplotlib_rc(self):
+        text_color = "#222222"
+        axis_color = "#cccccc"
+
+        return {
+            "lines.linewidth": 3,
+            "text.color": text_color,
+            "axes.edgecolor": axis_color,
+            "axes.titlecolor": text_color,
+            "axes.labelcolor": text_color,
+            "axes.spines.top": False,
+            "axes.spines.right": False,
+            "xtick.labelsize": 8,
+            "xtick.color": text_color,
+            "xtick.direction": "in",
+            "ytick.labelsize": 8,
+            "ytick.color": text_color,
+            "ytick.direction": "in",
+            "date.autoformatter.day": "%b %-d\n(%a)",
+            "figure.dpi": 300,
+            "savefig.bbox": "tight",
+            "savefig.pad_inches": 0,
+        }
+
     def render(self, env):
         df = self.datasources.query(self.datasource, self.query)
         df = df.set_index(df.columns[0])
-        fig, ax = plt.subplots(nrows=1, ncols=1)
-        df.plot(ax=ax)
 
-        figbytes = io.BytesIO()
-        fig.savefig(figbytes)
-        figname = "figure.png"
-        self.add_blob(figname, figbytes)
+        with plt.rc_context(self.matplotlib_rc):
+            ax = df.plot(figsize=[self.cols, 2], legend=None, title=None)
+            ax.set_xlabel("")
 
-        plt.close(fig)
+            fig = ax.get_figure()
+            figbytes = io.BytesIO()
+            fig.savefig(figbytes)
+            figname = "figure2.png"
+            self.add_blob(figname, figbytes)
+
+            plt.close(fig)
 
         template = env.get_template("plot.html")
 

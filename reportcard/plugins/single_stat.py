4d94799550068999dbeff783f9fd28881d881409
Jason <diurnalist@gmail.com>
2020-05-04 21:15:24 -0500

    Move single_stat into own module

---
+++ reportcard/plugins/single_stat.py
@@ -0,0 +1,42 @@
+from reportcard.view import View
+
+
+class SingleStat(View):
+    def init(self, datasource=None, query=None, label="{stat}",
+             comparison_query=None, comparison_type="raw"):
+        self.datasource = datasource
+        self.query = query
+        self.label = label
+        self.comparison_query = comparison_query
+        self.comparison_type = comparison_type
+
+        if not (self.datasource and self.query):
+            raise ValueError((
+                "Both a 'datasource' and 'query' parameter are required"))
+
+    def render_html(self, j2):
+        df = self.datasources.query(self.datasource, self.query)
+        stat_value = df.index.array[0]
+        stat_delta = None
+        stat_delta_direction = None
+
+        if self.comparison_query:
+            df_cmp = self.datasources.query(self.datasource,
+                                            self.comparison_query)
+            stat_cmp_value = df_cmp.index.array[0]
+            stat_delta = stat_value - stat_cmp_value
+            stat_delta_direction = "up" if stat_delta >= 0 else "down"
+
+            if self.comparison_type == "percent":
+                if stat_cmp_value == 0:
+                    # Avoid divide by zero
+                    stat_delta = None
+                else:
+                    stat_delta = f"{(stat_delta / stat_cmp_value) * 100:.1f}%"
+
+        label = self.label.format(stat=stat_value)
+        template = j2.get_template("plugins/single_stat.html")
+
+        return template.render(theme=self.report.theme, label=label,
+                               stat_delta=stat_delta,
+                               direction=stat_delta_direction)

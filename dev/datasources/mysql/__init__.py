d80e2af1e60bd6a577bdf77af6a3d907fa913433
Jason <diurnalist@gmail.com>
2020-05-16 13:48:57 -0500

    Generate synthetic Prometheus metrics
    Maintaining a gigantic Prometheus metric dump is annoying. Arguably more
    annoying is generating something resembling a realistic data set
    synthetically. But, it's less lines of code in the repo, and it's more
    fun.

--- dev/datasources/mysql/__init__.py
+++ dev/datasources/mysql/__init__.py
@@ -2,12 +2,18 @@
 from datetime import datetime, timedelta
 import os
 
-from ..generate_timeseries import generate
+from ..generate_timeseries import generate_organic
 
 dirpath = os.path.dirname(os.path.realpath(__file__))
 
+
 def open_file(name, mode):
     return open(os.path.join(dirpath, name), mode)
+
+
+def mysql_date_format(datestr):
+    return datetime.strftime(datestr, format='%Y-%m-%d %H:%M:%S')
+
 
 def make():
     end_date = datetime.now()
@@ -16,8 +22,10 @@ def make():
 
     with open_file("signups.sql.tpl", "r") as f:
         template = f.read()
-        ts_data = generate(start_date=start_date, end_date=end_date)
-        values = [f'("{v}")' for v in ts_data]
-        values = f'{",".join(values)};'
+        ts_data = generate_organic(start_date=start_date, end_date=end_date)
+        values = [
+            f'("{mysql_date_format(v)}")'
+            for v in ts_data[ts_data['value']]['time']
+        ]
     with open_file("signups.sql", "w") as f:
-        f.write(template.format(values=values))
+        f.write(template.format(values=f'{",".join(values)};'))
--- dev/datasources/prometheus/__init__.py
+++ dev/datasources/prometheus/__init__.py
@@ -1,39 +1,67 @@
 import json
 import os
 
+from ..generate_timeseries import generate_sinusoidal
+
 dirpath = os.path.dirname(os.path.realpath(__file__))
+
 
 def open_file(name, mode):
     return open(os.path.join(dirpath, name), mode)
 
+
+def to_prom_values(ts_df):
+    ts_df['time'] = ts_df['time'].astype(int) / 10**9
+    ts_df['value'] = ts_df['value'].astype(str)
+    return ts_df.to_numpy().tolist()
+
+
 def make():
     expectations = []
 
-    with open_file("prometheus_alerts_mock.json", "r") as f:
-        expectations.append({
-            "httpRequest": {
-                "path": "/api/v1/query_range",
-                "queryStringParameters": {
-                    "query": [".*ALERTS.*"]
-                }
-            },
-            "httpResponse": {
-                "body": json.load(f)
-            }
+    # with open_file("prometheus_alerts_mock.json", "r") as f:
+    #     expectations.append({
+    #         "httpRequest": {
+    #             "path": "/api/v1/query_range",
+    #             "queryStringParameters": {
+    #                 "query": [".*ALERTS.*"]
+    #             }
+    #         },
+    #         "httpResponse": {
+    #             "body": json.load(f)
+    #         }
+    #     })
+
+    def make_response():
+        return dict(status="success", data={
+            "resultType": "matrix",
+            "result": []
         })
 
-    with open_file("prometheus_node_cpu_seconds_mock.json", "r") as f:
-        expectations.append({
-            "httpRequest": {
-                "path": "/api/v1/query_range",
-                "queryStringParameters": {
-                    "query": [".*node_cpu_seconds_total.*"]
-                }
-            },
-            "httpResponse": {
-                "body": json.load(f)
+    def add_result(response, labels, values):
+        response["data"]["result"].append(dict(
+            metric=labels,
+            values=values
+        ))
+
+    def add_sinusoid_result(res, hostname, **generate_kwargs):
+        ts = generate_sinusoidal(**generate_kwargs)
+        return add_result(res, {"hostname": hostname}, to_prom_values(ts))
+
+    node_cpu_response = make_response()
+    add_sinusoid_result(node_cpu_response, "app01", base=0, amplitude=12)
+    add_sinusoid_result(node_cpu_response, "db01", base=4, amplitude=0.5)
+    expectations.append({
+        "httpRequest": {
+            "path": "/api/v1/query_range",
+            "queryStringParameters": {
+                "query": [".*node_cpu_seconds_total.*"]
             }
-        })
+        },
+        "httpResponse": {
+            "body": node_cpu_response
+        }
+    })
 
     with open_file("prometheus_api_mock.json", "w") as f:
         json.dump(expectations, f)

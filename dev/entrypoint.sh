18e08bd52fa2b3e928b6911c8826a26fc254dc50
Jason Anderson <diurnalist@gmail.com>
2020-09-12 17:45:50 -0500

    Fix bash lint errors

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -2,9 +2,7 @@
 # The entrypoint for the "dev" Docker image target ensures that the locally-
 # mounted code for kpireport and the plugins are installed and linked.
 pip install -e .
-for plugin in $(find plugins -maxdepth 1 -mindepth 1 -type d); do
-  pip install -e $plugin
-done
+find plugins -maxdepth 1 -mindepth 1 -type d -exec pip install -e {} \;
 echo 1 >/root/kpireport_ready_signal
 # Start a new interactive shell
 exec bash

dac11845e8f2e88d272ae852945ec00d1f1509f2
Jason Anderson <diurnalist@gmail.com>
2020-10-17 18:56:12 -0500

    Fix issue with table docs

--- plugins/table/kpireport_table/table.py
+++ plugins/table/kpireport_table/table.py
@@ -2,21 +2,30 @@ from functools import lru_cache
 
 from kpireport.view import View
 
+DEFAULT_MAX_ROWS = 10
+
 
 class Table(View):
     """Render data fetched from a datasource as a table.
+
+    A table can be rendered as HTML, Markdown, or for display in :ref:`Slack
+    <slack-plugin>`. When displayed for Slack, the table is rendered as an
+    image, as Slack does not have support for rendering tables as part of a
+    message block.
 
     Attributes:
         datasource (str): the datasource ID to fetch from.
         query (str): the query to execute against the datasource.
         query_args (dict): any additional keyword arguments to the datasource
             query operation.
+        max_rows (int): maximum number of rows to display. If the output table
+            has more rows, they are ignored. (Default 10)
     """
     def init(self, datasource=None, query=None, query_args=None, max_rows=None):
         self.datasource = datasource
         self.query = query
         self.query_args = query_args or {}
-        self.max_rows = max_rows
+        self.max_rows = max_rows or DEFAULT_MAX_ROWS
 
         if not (self.datasource and self.query):
             raise ValueError(("Both a 'datasource' and 'query' parameter are required"))
@@ -53,7 +62,10 @@ class Table(View):
         return f"{styles}\n{table}"
 
     def render_md(self, j2):
-        return self._query().to_markdown()
+        # TODO(jason): to support RST, use tablefmt="rst"
+        # The user will have to indicate they want RST flavor.
+        # (Or: just use pandoc for translation?)
+        return self._query().to_markdown(tablefmt="grid")
 
     # def render_slack(self, j2):
         # Render as image?

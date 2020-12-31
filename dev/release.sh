4021be8fdf55fc5a65b202d9306f02f561f649e2
Jason <diurnalist@gmail.com>
2020-12-31 17:27:47 -0600

    Fixup reno when abspath used for relnotesdir
    Not sure exactly why this happens, probably some Git black
    magic elsewhere within reno.

--- dev/release.sh
+++ dev/release.sh
@@ -51,8 +51,8 @@ publish_path() {
 }
 
 publish_plugin() {
-  local current_tag="$(git tag -l "$1/*" | tail -n1)"
-  local next_tag="$(reno -q --rel-notes-dir $1/releasenotes semver-next)"
+  local current_tag="$(git tag -l "$(basename $1)/*" | tail -n1)"
+  local next_tag="$(reno -q --rel-notes-dir $(realpath --relative-to="$PWD" "$1/releasenotes") semver-next)"
   publish_path "$1" "$current_tag" "$next_tag" setup.py
 }
 

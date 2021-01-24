97e519bde7f9fbe2f8558b54b0a545c4f1a51966
Jason <diurnalist@gmail.com>
2021-01-23 19:16:25 -0600

    slack: 0.0.3

--- plugins/slack/setup.py
+++ plugins/slack/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="kpireport-slack",
-    version="0.0.2",
+    version="0.0.3",
     author="KPI Reporter LLC",
     author_email="dev@kpireporter.com",
     url="https://kpireporter.com",

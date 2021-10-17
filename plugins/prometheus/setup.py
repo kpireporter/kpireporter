32c968b99d271bfc14b9a5525a4c3fad11dfb785
Jason <diurnalist@gmail.com>
2021-10-17 17:47:06 -0500

    prometheus: 0.0.2

--- plugins/prometheus/setup.py
+++ plugins/prometheus/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="kpireport-prometheus",
-    version="0.0.1",
+    version="0.0.2",
     author="KPI Reporter LLC",
     author_email="dev@kpireporter.com",
     url="https://kpireporter.com",

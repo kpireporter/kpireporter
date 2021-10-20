649ba8af98d0f96f87fbc12a9aba94efc4a8e023
Jason <diurnalist@gmail.com>
2021-10-19 20:33:31 -0500

    plot: 0.3.0

--- plugins/plot/setup.py
+++ plugins/plot/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="kpireport-plot",
-    version="0.2.2",
+    version="0.3.0",
     author="KPI Reporter LLC",
     author_email="dev@kpireporter.com",
     url="https://kpireporter.com",

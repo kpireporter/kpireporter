15d8f8a929621d830b7c51fa3c0d65453e0012b1
Jason <diurnalist@gmail.com>
2022-10-04 17:02:18 -0500

    Add constraint on matplotlib dependency

--- plugins/plot/setup.py
+++ plugins/plot/setup.py
@@ -8,7 +8,7 @@ setup(
     url="https://kpireporter.com",
     license="Prosperity Public License",
     packages=["kpireport_plot"],
-    install_requires=["kpireport", "matplotlib"],
+    install_requires=["kpireport", "matplotlib~=3.4"],
     package_data={"kpireport_plot": ["templates/*"]},
     entry_points={
         "kpireport.view": [
